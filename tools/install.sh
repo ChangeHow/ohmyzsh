de5a306adf37b0d8ff64261e3abe031e362785d6
Yad Smood <y.s.inside@gmail.com>
2015-01-21 11:32:21 +0800

    upd

--- tools/install.sh
+++ tools/install.sh
@@ -50,47 +50,10 @@ HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
 HOME="${HOME:-$(eval echo ~$USER)}"
 
 
-# Track if $ZSH was provided
-custom_zsh=${ZSH:+yes}
-
-# Default settings
-ZSH="${ZSH:-$HOME/.oh-my-zsh}"
-REPO=${REPO:-ohmyzsh/ohmyzsh}
-REMOTE=${REMOTE:-https://github.com/${REPO}.git}
-BRANCH=${BRANCH:-master}
-
-# Other options
-CHSH=${CHSH:-yes}
-RUNZSH=${RUNZSH:-yes}
-KEEP_ZSHRC=${KEEP_ZSHRC:-no}
-
-
-command_exists() {
-  command -v "$@" >/dev/null 2>&1
-}
-
-user_can_sudo() {
-  # Check if sudo is installed
-  command_exists sudo || return 1
-  # The following command has 3 parts:
-  #
-  # 1. Run `sudo` with `-v`. Does the following:
-  #    • with privilege: asks for a password immediately.
-  #    • without privilege: exits with error code 1 and prints the message:
-  #      Sorry, user <username> may not run sudo on <hostname>
-  #
-  # 2. Pass `-n` to `sudo` to tell it to not ask for a password. If the
-  #    password is not required, the command will finish with exit code 0.
-  #    If one is required, sudo will exit with error code 1 and print the
-  #    message:
-  #    sudo: a password is required
-  #
-  # 3. Check for the words "may not run sudo" in the output to really tell
-  #    whether the user has privileges or not. For that we have to make sure
-  #    to run `sudo` in the default locale (with `LANG=`) so that the message
-  #    stays consistent regardless of the user's locale.
-  #
-  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+echo "\033[0;34mCloning Oh My Zsh...\033[0m"
+hash git >/dev/null 2>&1 && env git clone https://github.com/ysmood/oh-my-zsh.git $ZSH || {
+  echo "git not installed"
+  exit
 }
 
 # The [ -t 1 ] check only works when the function is not called from
